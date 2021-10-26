322524c1f436901f5a3006aa25869b456d84ea81
Hammer922 <92076627+Hammer922@users.noreply.github.com>
2021-10-26 14:48:32 +0100

    total function work needed

--- counter/index.js
+++ counter/index.js
@@ -1,5 +1,6 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
 //let resetEl = document.getElementById("reset-el")
 let count = 0
 
@@ -7,18 +8,20 @@ console.log(saveEl)
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
     let countStr = count + " - "
-    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    // 3. Render the variable in the saveEl using innerText
     saveEl.textContent += countStr
-    // NB: Make sure to not delete the existing content of the paragraph
+    totalEl.textContent += count + count // Total Element Needs Work //
+    count = 0
+    countEl.textContent = 0
+    
     console.log(count)
 }
 function reset() {
     count = 0
-    countEl.innerText = 0
+    countEl.textContent = 0
 }
+
