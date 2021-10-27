97c5975c378f4d6573686a8cc7b4760f4e72b7c8
Hammer922 <92076627+Hammer922@users.noreply.github.com>
2021-10-27 22:13:14 +0100

    bit lost tonight

---
+++ practice/index.js
@@ -0,0 +1,15 @@
+let onPress1 = document.getElementById("btn-1")
+let onPress2 = document.getElementById("btn-2")
+let names = "Chris Hammond"
+let count = 0
+
+function button1() {
+    count += 1
+    onPress1.textContent = count
+    //console.log(count)
+}
+
+function button2() {
+    count += 1
+    onPress2.textContent = names + " " + count
+}
